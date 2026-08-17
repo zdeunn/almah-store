afbfa9ab2ad7c87a5c676a550d3414caeb3f47ed
zdeunn <zzidane915@gmail.com>
2026-08-17 16:48:44 +0100

    fix: Database file

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,31 +1,27 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-load_dotenv()  # take environment variables from .env file
-# URL to connect with database (from .env file) + the URL formula
-DATABASE_URL = os.getenv(
-    "DATABASE_URL"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
 )
 
-# the engine manage the connect with databse, echo=True for print the query and run them in terminal
-engine = create_async_engine(
-    DATABASE_URL, 
-    echo=True,
-    connect_args={"prepared_statement_cache_size": 0} 
-    )
-
-# creating sessioons when we want to
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
 
-# all models have inherit from this class
+
 class Base(DeclarativeBase):
     pass
+
 
 async def get_db():
     async with AsyncSessionLocal() as session:
